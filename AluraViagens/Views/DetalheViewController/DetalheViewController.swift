be1e839806a94b2b1911ee26e6a8cac6cb84a315
skiterandre <38163435+skiterandre@users.noreply.github.com>
2022-02-24 19:57:15 -0300

    Tela de detalhes

--- AluraViagens/Views/DetalheViewController/DetalheViewController.swift
+++ AluraViagens/Views/DetalheViewController/DetalheViewController.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class DetalheViewController: UIViewController {
 
+    //MARK: - IBOutlets
+    @IBOutlet weak var viagemImage: UIImageView!
+    @IBOutlet weak var tituloViagemLabel: UILabel!
+    @IBOutlet weak var subtituloViagemLabel: UILabel!
 
-
+    
+    //MARK: - View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    //MARK: - Actions
+    @IBAction func botaoVoltar(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
+    
 }
